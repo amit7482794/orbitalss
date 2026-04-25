25777b2c1ff0c007f9af8b9e56c85e24c18c4db1
amit7482794 <pyushr622@gmail.com>
2026-04-25 15:01:39 +0530

    project setup with Next.js and basic UI

---
+++ app/page.tsx
@@ -0,0 +1,20 @@
+ import styles from "./page.module.css";
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <div>
+        <h1 className={styles.title}>HI 
+          <br />       I AM AMIT </h1>
+        
+        <p className={styles.text}> i am the founder and CEO of the ORBITALS FOUNDATION</p>
+        <button className={styles.login}>Login</button>
+      <button className={styles.signup}>Sign Up</button>
+      </div>
+       
+    </div>
+    //
+  );
+}
+//import styles from './navbar.module.css';
+
+ 
